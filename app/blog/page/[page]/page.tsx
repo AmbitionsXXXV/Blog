7a0ec7079013ff285789f957f4b114d43b18f73d
etctera <yilonglei790@gmail.com>
2023-11-08 15:57:47 +0800

    chore: page show number

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -2,15 +2,14 @@ import ListLayout from '@/layouts/ListLayoutWithTags'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 
-const POSTS_PER_PAGE = 5
+const POSTS_PER_PAGE = 10
 
 export const generateStaticParams = async () => {
   const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
-  const paths = Array.from({ length: totalPages }, (_, i) => ({
+
+  return Array.from({ length: totalPages }, (_, i) => ({
     page: (i + 1).toString(),
   }))
-
-  return paths
 }
 
 export default function Page({ params }: { params: { page: string } }) {
